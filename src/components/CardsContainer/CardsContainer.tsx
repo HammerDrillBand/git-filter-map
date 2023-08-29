a289c98181df6458439705f83653a02837e8d12b
Avigail Geisler <avigail.geisler@clarivate.com>
2023-08-29 20:13:41 +0300

    added filter

--- src/components/CardsContainer/CardsContainer.tsx
+++ src/components/CardsContainer/CardsContainer.tsx
@@ -5,34 +5,35 @@ import './CardsContainer.css'
 
 
 function CardsContainer() {
-    let [productsModel, setProductsModel] = useState<IProduct[]>([{ id: 0, name: 'Computer Screen', description: '26" 4K LED display', company: 'LG', price: 1200, amount: 7},
-    { id: 1, name: 'Whiskey', description: 'Blended scotch whiskey', company: 'Jack Daniels', price: 100, amount: 50},
-    { id: 2, name: 'The third product', description: 'Some random thing. I could not think of anything original', company: 'The one and only', price: 200, amount: 13}]);
+    let [productsModel, setProductsModel] = useState<IProduct[]>([{ id: 0, name: 'Computer Screen', description: '26" 4K LED display', company: 'LG', price: 1200, amount: 7 },
+    { id: 1, name: 'Whiskey', description: 'Blended scotch whiskey', company: 'Jack Daniels', price: 100, amount: 50 },
+    { id: 2, name: 'The third product', description: 'Some random thing. I could not think of anything original', company: 'The one and only', price: 200, amount: 13 }]);
 
     let [productsViewModel, setProductsViewModel] = useState<IProduct[]>(productsModel);
     let [userType, setUserType] = useState('user');
     let [searchText, setSearchText] = useState("");
 
-    function deleteProduct(deletedProductId:number): void{
+    function deleteProduct(deletedProductId: number): void {
         productsViewModel = productsViewModel.filter(product => product.id != deletedProductId);
         setProductsViewModel(productsViewModel);
     }
 
     function changeUserType() {
         debugger;
-        setUserType('admin')       
+        setUserType('admin')
     }
 
-    function isShowProduct(product: IProduct): boolean{
-        if (searchText == ""){
+    function isShowProduct(product: IProduct): boolean {
+        if (searchText == "") {
             return true;
         }
 
-        return product.name.toLowerCase().includes(searchText);       
+        return product.name.toLowerCase().includes(searchText);
     }
 
     return (
         <div className="CardsContainer">
+            {productsViewModel.filter(product => isShowProduct(product))}
             {/* {productsViewModel
             .filter(product => isShowProduct(product))
             .map((product) =>
@@ -40,8 +41,8 @@ function CardsContainer() {
             )} */}
             <button onClick={() => changeUserType()}>Change to Admin</button>
             <button onClick={() => setProductsViewModel(productsModel)}>Unhide all products</button>
-            <br/>
-            <input type="text" placeholder="Search" onChange={event => setSearchText(event.target.value.toLowerCase())}/>
+            <br />
+            <input type="text" placeholder="Search" onChange={event => setSearchText(event.target.value.toLowerCase())} />
         </div>
     );
 }
